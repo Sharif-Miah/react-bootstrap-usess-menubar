6401894843db4bcc204784aaf5ceb22a8dbbb374
farhan sharif <farhansharif1410@gmail.com>
2022-10-01 17:59:11 +0530

    cart added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
+
 import './App.css';
 import Carosel from './component/Carosel/Carosel';
 import Navber from './component/Navbar/Navber';
+import Cart from './component/Cart/Cart';
 
 function App() {
   return (
     <div className="App">
       <Navber />
       <Carosel />
+      <Cart />
     </div>
   );
 }
